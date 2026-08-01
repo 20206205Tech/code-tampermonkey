19b607ba09afbe99229680efafaec106391f0139
vuvannghiawork <vuvannghia.work@gmail.com>
2026-08-01 12:09:55 +0700

    1

---
+++ main.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      2026-08-01
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.facebook.com/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=facebook.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    alert("V1");
+})();
